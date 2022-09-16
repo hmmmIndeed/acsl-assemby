59440ba688ce66496d9dbe83b743fbd4cc41c1b8
hmmmIndeed <73439762+hmmmIndeed@users.noreply.github.com>
2022-09-16 11:41:09 -0400

    Update Executor.java

--- Executor.java
+++ Executor.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Executor {
   
+  
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    String line = input.nextLine();
+    
+  }
 }
